dd61ae36e671ddf2ca79d88543939eaa1f1295f5
jacob-fry85 <darwin.hermawan@gmail.com>
2022-11-14 11:22:34 +0700

    first commit

--- src/main/java/com/jacobfrye/spring_boot_aws_rds/SpringBootAwsRdsApplication.java
+++ src/main/java/com/jacobfrye/spring_boot_aws_rds/SpringBootAwsRdsApplication.java
@@ -2,8 +2,10 @@ package com.jacobfrye.spring_boot_aws_rds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootAwsRdsApplication {
 
     public static void main(String[] args) {
